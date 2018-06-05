92e0a46bf999ccac7b252f84ff2a6b372b29ee2c
akhapangee <akhapangee@gmail.com>
2018-06-05 22:22:10 +0545

    First commit

---
+++ src/main/java/com/akhilesh/admin/DashboardController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akhilesh.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Akhilesh
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class DashboardController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "admin/dashboard/index";
+    }
+}
